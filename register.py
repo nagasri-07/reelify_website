4766d423e0eb5bdb28a6f498fba3cc01a22c73c9
nagasri-07 <yervanagasri@gmail.com>
2025-08-09 22:23:05 +0530

    Update register.py

--- register.py
+++ register.py
@@ -11,7 +11,7 @@ import re
 # --- CONFIGURE GEMINI ---
 API_KEY = "AIzaSyDou_lfKmdoC19dqpolzGY6EY7eUskkgUI"
 genai.configure(api_key=API_KEY)
-gemini_model = genai.GenerativeModel("gemini-2.5-pro")
+gemini_model = genai.GenerativeModel("gemini-2.5-flash")
 
 # --- DB Connection ---
 def get_connection():
